dfa8745adb37a7802954a0fa67a88454b07cb293
simonvangorp <simonvgorp@gmail.com>
2024-11-28 22:22:41 +0100

    update requirements.txt

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 Toni rullleez")
+st.title("🎈 Toni rullleez bro")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
